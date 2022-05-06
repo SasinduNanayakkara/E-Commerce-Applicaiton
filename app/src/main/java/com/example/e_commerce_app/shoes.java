22e514d050f2a9f29aa6a9606109526c70e82d1a
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-05-06 23:52:34 +0530

    initialize ids and retrieve data

--- app/src/main/java/com/example/e_commerce_app/shoes.java
+++ app/src/main/java/com/example/e_commerce_app/shoes.java
@@ -1,14 +1,64 @@
 package com.example.e_commerce_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class shoes extends AppCompatActivity {
+
+    ImageView shoe1, shoe2, shoe3, shoe4, back, home, cart, profile;
+    TextView price1, price2, price3, price4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shoes);
+
+        shoe1 = findViewById(R.id.shoeImage1);
+        shoe2 = findViewById(R.id.shoeImage2);
+        shoe3 = findViewById(R.id.shoeImage3);
+        shoe4 = findViewById(R.id.shoeImage4);
+        back = findViewById(R.id.shoePageBack);
+        home = findViewById(R.id.homePageIcon);
+        cart = findViewById(R.id.cartIcon);
+        profile = findViewById(R.id.profileIcon);
+        price1 = findViewById(R.id.shoePrice1);
+        price2 = findViewById(R.id.shoePrice2);
+        price3 = findViewById(R.id.shoePrice3);
+        price4 = findViewById(R.id.shoePrice4);
+
+        final ArrayList<String> shoeList = new ArrayList<>();
+        final ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_shoes, shoeList);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("honeyBeeDB").child("Products");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                shoeList.clear();
+                for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    shoeList.add(dataSnapshot.getValue().toString());
+                }
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 }
