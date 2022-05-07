33dec63f7e8e30c25238d98a9d33ac9854ed081e
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-05-08 03:13:09 +0530

    create product model

---
+++ app/src/main/java/com/example/e_commerce_app/Product.java
@@ -0,0 +1,90 @@
+package com.example.e_commerce_app;
+
+import android.os.Parcel;
+
+public class Product {
+    private String itemName;
+    private String itemCode;
+    private String description;
+    private String category;
+    private String imageUrl;
+    private String price;
+    private String size;
+
+    public Product(String itemName, String itemCode, String description, String category, String imageUrl, String price, String size) {
+        this.itemName = itemName;
+        this.itemCode = itemCode;
+        this.description = description;
+        this.category = category;
+        this.imageUrl = imageUrl;
+        this.price = price;
+        this.size = size;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(itemName);
+        dest.writeString(itemCode);
+        dest.writeString(description);
+        dest.writeString(category);
+        dest.writeString(imageUrl);
+        dest.writeString(price);
+        dest.writeString(size);
+
+    }
+}
