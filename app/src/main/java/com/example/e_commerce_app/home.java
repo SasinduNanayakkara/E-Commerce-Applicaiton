fd65cfa8ac0c640f75d7f683bcc003f82989da04
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-05-08 03:12:22 +0530

    initialize home ids and set intents

--- app/src/main/java/com/example/e_commerce_app/home.java
+++ app/src/main/java/com/example/e_commerce_app/home.java
@@ -1,14 +1,87 @@
 package com.example.e_commerce_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class home extends AppCompatActivity {
+
+    ImageView frockImage, tShirtImage, trouserImage, jeweleryImage, shoeImage, bagImage, cartIcon, profileIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        frockImage = findViewById(R.id.FrockImageIcon);
+        tShirtImage = findViewById(R.id.TShirtImageIcon);
+        trouserImage = findViewById(R.id.TrouserImageIcon);
+        jeweleryImage = findViewById(R.id.JewelleryImageIcon);
+        shoeImage = findViewById(R.id.ShoeImageIcon);
+        bagImage = findViewById(R.id.BagImageIcon);
+        cartIcon = findViewById(R.id.CartImageIcon);
+        profileIcon = findViewById(R.id.profileImageIcon);
+
+        frockImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), frock.class);
+                startActivity(intent);
+            }
+        });
+
+        tShirtImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), tshirts.class);
+                startActivity(intent);
+            }
+        });
+
+        trouserImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), trousers.class);
+                startActivity(intent);
+            }
+        });
+
+        jeweleryImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), jewelry.class);
+                startActivity(intent);
+            }
+        });
+
+        shoeImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), shoes.class);
+                startActivity(intent);
+            }
+        });
+
+        bagImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), shoes.class);
+                startActivity(intent);
+            }
+        });
     }
 }
