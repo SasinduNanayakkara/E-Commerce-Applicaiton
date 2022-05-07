f4c96a0874afb759472e4f9ea699e23387e27bda
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-05-07 18:30:55 +0530

    change intent class

--- app/src/main/java/com/example/e_commerce_app/MainActivity.java
+++ app/src/main/java/com/example/e_commerce_app/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), shoes.class);
+                Intent intent = new Intent(getApplicationContext(), sample.class);
                 startActivity(intent);
             }
         });
