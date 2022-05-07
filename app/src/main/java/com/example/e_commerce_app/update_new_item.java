334c4497f3a421136a62293129e3fe7576531de9
User <navodhya2011@gmail.com>
2022-05-07 11:43:03 +0530

    update_new_item

---
+++ app/src/main/java/com/example/e_commerce_app/update_new_item.java
@@ -0,0 +1,14 @@
+package com.example.e_commerce_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class update_new_item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_new_item);
+    }
+}
