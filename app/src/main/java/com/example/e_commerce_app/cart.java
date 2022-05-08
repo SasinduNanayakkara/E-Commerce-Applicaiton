065d7f06b638f10896b0925385228809f26e9cac
User <navodhya2011@gmail.com>
2022-05-08 21:10:44 +0530

    Cart implementation

--- app/src/main/java/com/example/e_commerce_app/cart.java
+++ app/src/main/java/com/example/e_commerce_app/cart.java
@@ -3,12 +3,28 @@ package com.example.e_commerce_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class cart extends AppCompatActivity {
+    Button add;
+    TextView cartQuantity, cartQuantity1, cartQuantity2, cartButton1, cartButton2, cartButton3;
+    ImageView cartDeleteIcon, cartDeleteIcon1, cartDeleteIcon2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+        add = findViewById(R.id.cartButton1);
+        add = findViewById(R.id.cartButton2);
+        add = findViewById(R.id.cartButton3);
+        cartQuantity = findViewById(R.id.cartQuantity);
+        cartQuantity1 = findViewById(R.id.cartQuantity1);
+        cartQuantity2 = findViewById(R.id.cartQuantity2);
+        cartDeleteIcon = findViewById(R.id.cartDeleteIcon);
+        cartDeleteIcon1 = findViewById(R.id.cartDeleteIcon1);
+        cartDeleteIcon2 = findViewById(R.id.cartDeleteIcon2);
     }
 }
