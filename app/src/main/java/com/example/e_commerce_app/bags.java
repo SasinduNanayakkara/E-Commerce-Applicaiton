bfbb073beae1f67b81fbbdebed8c5ee34079148e
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-05-10 04:00:55 +0530

    set bag view functionality

--- app/src/main/java/com/example/e_commerce_app/bags.java
+++ app/src/main/java/com/example/e_commerce_app/bags.java
@@ -8,7 +8,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,6 +25,8 @@ public class bags extends AppCompatActivity {
 
     ImageView bagImage1, bagImage2, bagImage3, bagImage4, home, back, cart, profile;
     TextView bagPrice1, bagPrice2, bagPrice3, bagPrice4;
+    DatabaseReference reference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,27 +46,136 @@ public class bags extends AppCompatActivity {
         bagPrice3 = findViewById(R.id.bag3Price);
         bagPrice4 = findViewById(R.id.bag4price);
 
-        final ArrayList<String> bahList = new ArrayList<>();
+        String item = "Bags";
 
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("honeyBeeDB").child("products");
-        reference.addValueEventListener(new ValueEventListener() {
+        readData(item);
+
+        bagImage1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for(DataSnapshot dataSnapshot: snapshot.getChildren()){
-                    if (((HashMap)(dataSnapshot.getValue())).get("category") == "Frocks") {
-                        bahList.add(dataSnapshot.getValue().toString());
+            public void onClick(View view) {
+                reference = FirebaseDatabase.getInstance().getReference("Products");
+                reference.child(item).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DataSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DataSnapshot dataSnapshot = task.getResult();
+                            Product product = new Product();
+
+                            product.setItemName(String.valueOf(dataSnapshot.child("B100").child("itemName").getValue()));
+                            product.setItemCode(String.valueOf(dataSnapshot.child("B100").child("itemCode").getValue()));
+                            product.setPrice(String.valueOf(dataSnapshot.child("B100").child("price").getValue()));
+                            product.setSize(String.valueOf(dataSnapshot.child("B100").child("size").getValue()));
+                            product.setDescription(String.valueOf(dataSnapshot.child("B100").child("description").getValue()));
+
+                            //productImage.setImageResource(R.drawable.frock1);
+                            Intent intent = new Intent(getApplicationContext(), admin_product_view.class);
+                            intent.putExtra("itemName", product.getItemName());
+                            intent.putExtra("itemCode", product.getItemCode());
+                            intent.putExtra("description", product.getDescription());
+                            intent.putExtra("price", product.getPrice());
+                            intent.putExtra("size", product.getSize());
+                            intent.putExtra("image", "bg1");
+                            startActivity(intent);
+
+                        }
                     }
-                }
-
-                for(int i=0; i<bahList.size(); i++) {
-
-                }
-
+                });
             }
+        });
 
+        bagImage2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+            public void onClick(View view) {
+                reference = FirebaseDatabase.getInstance().getReference("Products");
+                reference.child(item).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DataSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DataSnapshot dataSnapshot = task.getResult();
+                            Product product = new Product();
 
+                            product.setItemName(String.valueOf(dataSnapshot.child("B101").child("itemName").getValue()));
+                            product.setItemCode(String.valueOf(dataSnapshot.child("B101").child("itemCode").getValue()));
+                            product.setPrice(String.valueOf(dataSnapshot.child("B101").child("price").getValue()));
+                            product.setSize(String.valueOf(dataSnapshot.child("B101").child("size").getValue()));
+                            product.setDescription(String.valueOf(dataSnapshot.child("B101").child("description").getValue()));
+
+                            Intent intent = new Intent(getApplicationContext(), admin_product_view.class);
+                            intent.putExtra("itemName", product.getItemName());
+                            intent.putExtra("itemCode", product.getItemCode());
+                            intent.putExtra("description", product.getDescription());
+                            intent.putExtra("price", product.getPrice());
+                            intent.putExtra("size", product.getSize());
+                            intent.putExtra("image", "bg2");
+                            startActivity(intent);
+                        }
+                    }
+                });
+            }
+        });
+
+        bagImage3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reference = FirebaseDatabase.getInstance().getReference("Products");
+                reference.child(item).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DataSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DataSnapshot dataSnapshot = task.getResult();
+                            Product product = new Product();
+
+                            product.setItemName(String.valueOf(dataSnapshot.child("B102").child("itemName").getValue()));
+                            product.setItemCode(String.valueOf(dataSnapshot.child("B102").child("itemCode").getValue()));
+                            product.setPrice(String.valueOf(dataSnapshot.child("B102").child("price").getValue()));
+                            product.setSize(String.valueOf(dataSnapshot.child("B102").child("size").getValue()));
+                            product.setDescription(String.valueOf(dataSnapshot.child("B102").child("description").getValue()));
+
+                            Intent intent = new Intent(getApplicationContext(), admin_product_view.class);
+                            intent.putExtra("itemName", product.getItemName());
+                            intent.putExtra("itemCode", product.getItemCode());
+                            intent.putExtra("description", product.getDescription());
+                            intent.putExtra("price", product.getPrice());
+                            intent.putExtra("size", product.getSize());
+                            intent.putExtra("image", "bg5");
+
+                            startActivity(intent);
+                        }
+                    }
+                });
+            }
+        });
+
+        bagImage4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reference = FirebaseDatabase.getInstance().getReference("Products");
+                reference.child(item).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DataSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DataSnapshot dataSnapshot = task.getResult();
+                            Product product = new Product();
+
+                            product.setItemName(String.valueOf(dataSnapshot.child("B103").child("itemName").getValue()));
+                            product.setItemCode(String.valueOf(dataSnapshot.child("B103").child("itemCode").getValue()));
+                            product.setPrice(String.valueOf(dataSnapshot.child("B103").child("price").getValue()));
+                            product.setSize(String.valueOf(dataSnapshot.child("B103").child("size").getValue()));
+                            product.setDescription(String.valueOf(dataSnapshot.child("B103").child("description").getValue()));
+
+                            //productImage.setImageDrawable(getDrawable(R.drawable.frock5));
+                            Intent intent = new Intent(getApplicationContext(), admin_product_view.class);
+                            intent.putExtra("itemName", product.getItemName());
+                            intent.putExtra("itemCode", product.getItemCode());
+                            intent.putExtra("description", product.getDescription());
+                            intent.putExtra("price", product.getPrice());
+                            intent.putExtra("size", product.getSize());
+                            intent.putExtra("image", "bg8");
+
+                            startActivity(intent);
+                        }
+                    }
+                });
             }
         });
 
@@ -88,9 +202,26 @@ public class bags extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
 
-
-
-
+    private void readData(String item) {
+        reference = FirebaseDatabase.getInstance().getReference("Products");
+        reference.child(item).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (task.isSuccessful()) {
+                    if (task.getResult().exists()) {
+                        DataSnapshot dataSnapshot = task.getResult();
+                        bagPrice1.setText("LKR " + String.valueOf(dataSnapshot.child("B100").child("price").getValue()));
+                        bagPrice2.setText("LKR" + String.valueOf(dataSnapshot.child("B101").child("price").getValue()));
+                        bagPrice3.setText("LKR" + String.valueOf(dataSnapshot.child("B102").child("price").getValue()));
+                        bagPrice4.setText("LKR" + String.valueOf(dataSnapshot.child("B103").child("price").getValue()));
+                    }
+                }
+                else {
+                    Toast.makeText(bags.this, "Failed to read", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
