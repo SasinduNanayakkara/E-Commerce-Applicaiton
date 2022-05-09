a18987a52297a5f109a838f9f39b0a91a4b492e6
heshani9918 <it20606060@my.sliit.lk>
2022-05-10 04:48:52 +0530

    register java

--- app/src/main/java/com/example/e_commerce_app/register.java
+++ app/src/main/java/com/example/e_commerce_app/register.java
@@ -3,47 +3,38 @@ package com.example.e_commerce_app;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Dialog;
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import java.util.HashMap;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class register extends AppCompatActivity {
 
-    private Button button2;
-    private EditText RegisterName;
-    private EditText RegitserEmail;
-    private EditText RegisterphoneNumber;
-    private  EditText RegisterAddress;
-    private  EditText RegisterCountry;
-    private  EditText RegisterUsername;
-    private  EditText RegisterPassword;
+    Button add;
+    EditText RegisterName, RegisterEmail, RegisterphoneNumber, RegisterAddress,RegisterCountry,RegisterUsername, RegisterPassword;
+    FirebaseDatabase db;
+    Task<Void> reference;
 
     private FirebaseAuth auth;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        button2 = findViewById(R.id.button2);
+        add = findViewById(R.id.button2);
         RegisterName = findViewById(R.id.RegisterName);
-        RegitserEmail = findViewById(R.id.RegitserEmail);
+        RegisterEmail = findViewById(R.id.RegitserEmail);
         RegisterphoneNumber = findViewById(R.id.RegisterphoneNumber);
         RegisterAddress = findViewById(R.id.RegisterAddress);
         RegisterCountry = findViewById(R.id.RegisterCountry);
@@ -53,54 +44,68 @@ public class register extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
 
 
-        button2.setOnClickListener(new View.OnClickListener() {
+        add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String registerName = RegisterName.getText().toString();
-                String registerEmail = RegitserEmail.getText().toString();
+                String registerEmail = RegisterEmail.getText().toString();
                 String registerPhoneNumber = RegisterphoneNumber.getText().toString();
                 String registerAddress = RegisterAddress.getText().toString();
                 String registerCountry = RegisterCountry.getText().toString();
                 String registerUsername = RegisterUsername.getText().toString();
                 String registerPassword = RegisterPassword.getText().toString();
 
-                if (registerName.isEmpty() || registerEmail.isEmpty() || registerPhoneNumber .isEmpty() || registerAddress.isEmpty() || registerCountry.isEmpty() || registerUsername.isEmpty() || registerPassword.isEmpty()) {
+                if (registerName.isEmpty() || registerEmail.isEmpty() || registerPhoneNumber.isEmpty() || registerAddress.isEmpty() || registerCountry.isEmpty() || registerUsername.isEmpty() || registerPassword.isEmpty()) {
                     Toast.makeText(register.this, "Empty input fields not allowed", Toast.LENGTH_SHORT).show();
-                } else if (registerPassword.length() < 6){
+                } else if (registerPassword.length() < 6) {
                     Toast.makeText(register.this, "password too short!", Toast.LENGTH_SHORT).show();
-                } else{
-                    registerUser( registerEmail, registerPassword);
-
-                    HashMap<String , Object> map = new HashMap<>();
-                    map.put("registerName" , registerName);
-                    map.put("registerEmail", registerEmail);
-                    map.put("registerPhoneNumber" , registerPhoneNumber);
-                    map.put("registerAddress" , registerAddress);
-                    map.put("registerCountry" , registerCountry);
-                    map.put("registerUsername" , registerUsername);
-                    map.put("registerPassword" , registerPassword);
-
-                    FirebaseDatabase.getInstance().getReference().child("honeyBeeDB").child("Users").push().updateChildren(map);
-                    Toast.makeText(register.this, "User added successfully", Toast.LENGTH_SHORT).show();
                 }
+                 else {
+                    registerUser(registerEmail, registerPassword);
 
+                    registers r = new registers();
+
+
+                    r.setRegisterName(registerName);
+                    r.setRegisterEmail(registerEmail);
+                    r.setRegisterPhoneNumber(registerPhoneNumber);
+                    r.setRegisterAddress(registerAddress);
+                    r.setRegisterCountry(registerCountry);
+                    r.setRegisterUsername(registerUsername);
+                    r.setRegisterPassword(registerPassword);
+                    r.setAdmin(false);
+
+                    db = FirebaseDatabase.getInstance();
+                    reference = db.getReference("Users").child(registerPassword).setValue(r).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            RegisterName.setText("");
+                            RegisterEmail.setText("");
+                            RegisterphoneNumber.setText("");
+                            RegisterAddress.setText("");
+                            RegisterCountry.setText("");
+                            RegisterUsername.setText("");
+                            RegisterPassword.setText("");
+
+                            Toast.makeText(register.this, "New user added Successfully", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                }
+            }
+
+            private void registerUser(String registerEmail, String registerPassword) {
+
+                auth.createUserWithEmailAndPassword(registerEmail, registerPassword).addOnCompleteListener(register.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(register.this, "Registering user successfull !", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(register.this, "Registering failed !", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
-
-    }
-
-
-    private void registerUser(String registerEmail, String registerPassword) {
-
-        auth.createUserWithEmailAndPassword(registerEmail,registerPassword).addOnCompleteListener(register.this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()){
-                    Toast.makeText(register.this, "Registering user successfull !", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(register.this, "Registering failed !", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
-}
+    }}
