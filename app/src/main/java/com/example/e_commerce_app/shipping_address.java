0bcab31f110fa827f13ae399e6ad082f34edba6b
User <navodhya2011@gmail.com>
2022-05-10 08:43:06 +0530

    shipping_address temp

--- app/src/main/java/com/example/e_commerce_app/shipping_address.java
+++ app/src/main/java/com/example/e_commerce_app/shipping_address.java
@@ -1,11 +1,20 @@
 package com.example.e_commerce_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class shipping_address extends AppCompatActivity {
     Button add;
@@ -16,9 +25,35 @@ public class shipping_address extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shipping_address);
 
+        getSupportActionBar().hide();
+
         add = findViewById(R.id.EditAddressButton);
         NameN = findViewById(R.id.NameN);
         Address1 = findViewById(R.id.Address1);
         Address2 = findViewById(R.id.Address2);
+
+        final ArrayList<String> shippingAddressList = new ArrayList<>();
+        final ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_shoes, shippingAddressList);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("honeyBeeDB").child("Products");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    shippingAddressList.add(dataSnapshot.getValue().toString());
+
+                }
+                ArrayList<String> elements = new ArrayList<>();
+
+                adapter.notifyDataSetChanged();
+                final ArrayList<String> item = new ArrayList<>();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
