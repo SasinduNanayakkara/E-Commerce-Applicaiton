4a38259b797133d76ef9542a270cdef927f00bd2
User <navodhya2011@gmail.com>
2022-05-10 14:15:26 +0530

    shipping_address.java implement

--- app/src/main/java/com/example/e_commerce_app/shipping_address.java
+++ app/src/main/java/com/example/e_commerce_app/shipping_address.java
@@ -3,12 +3,17 @@ package com.example.e_commerce_app;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -16,12 +21,14 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class shipping_address extends AppCompatActivity {
     Button add;
     EditText Address;
     TextView Name;
 
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +41,31 @@ public class shipping_address extends AppCompatActivity {
         Address = findViewById(R.id.AddressInput);
         Name = findViewById(R.id.ShippingAddressName);
 
+        Name.setText(getIntent().getStringExtra("name"));
+        Address.setText(getIntent().getStringExtra("address"));
+        String password = getIntent().getStringExtra("password");
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateData(Address.getText().toString(), password);
+            }
+        });
 
 
+    }
+    private void updateData(String Address, String password) {
+        HashMap product  = new HashMap();
+        product.put("address", Address);
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users");
+        databaseReference.child(password).updateChildren(product).addOnCompleteListener(new OnCompleteListener() {
+            @Override
+            public void onComplete(@NonNull Task task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(shipping_address.this, "Data updated Successfully", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), home.class);
+                }
 
-
+            }
+        });
     }
 }
