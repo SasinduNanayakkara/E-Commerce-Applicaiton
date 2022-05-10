537b2bb8cd9390eb78835538d0f40912e22e89ee
User <navodhya2011@gmail.com>
2022-05-10 08:41:22 +0530

    cart temp implement

--- app/src/main/java/com/example/e_commerce_app/cart.java
+++ app/src/main/java/com/example/e_commerce_app/cart.java
@@ -1,11 +1,21 @@
 package com.example.e_commerce_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class cart extends AppCompatActivity {
     Button add;
@@ -17,6 +27,8 @@ public class cart extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
 
+        getSupportActionBar().hide();
+
         add = findViewById(R.id.cartButton1);
         add = findViewById(R.id.cartButton2);
         add = findViewById(R.id.cartButton3);
@@ -26,5 +38,29 @@ public class cart extends AppCompatActivity {
         cartDeleteIcon = findViewById(R.id.cartDeleteIcon);
         cartDeleteIcon1 = findViewById(R.id.cartDeleteIcon1);
         cartDeleteIcon2 = findViewById(R.id.cartDeleteIcon2);
+
+        final ArrayList<String> cartItems = new ArrayList<>();
+        final ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_shoes, cartItems);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("honeyBeeDB").child("Products");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    cartItems.add(dataSnapshot.getValue().toString());
+
+                }
+                ArrayList<String> elements = new ArrayList<>();
+
+                adapter.notifyDataSetChanged();
+                final ArrayList<String> item = new ArrayList<>();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
